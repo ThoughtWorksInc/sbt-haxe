5c165a5ce941a1ea12afb28bbe64c93e183c6093
Yang Bo <pop.atry@gmail.com>
2016-01-31 22:55:21 +0800

    Setup git_config in this repository instead of the secret repository

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-source ./secret/git_config.sh
+travis_retry git clone --depth=1 $secret_git ./secret
+cat ./secret/sonatype.sbt >> ./local.sbt
+
+git log --max-count=1 --format=format:%an | xargs -0 -n 1 git config --global -- user.name
+git log --max-count=1 --format=format:%ae | xargs -0 -n 1 git config --global -- user.email
+
+git config --global push.default simple
+
+git branch --force "$TRAVIS_BRANCH"
+git checkout "$TRAVIS_BRANCH"
+git config remote.origin.url git@github.com:"$TRAVIS_REPO_SLUG".git
+
+eval "$(ssh-agent -s)"
+ssh-add ./secret/id_rsa
+
 sbt "release with-defaults"
