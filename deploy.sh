b30345c7897e363a2afc4486495018aefabad00d
Yang Bo <pop.atry@gmail.com>
2016-01-31 22:58:06 +0800

    Let the deploy script fail fast

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-travis_retry git clone --depth=1 $secret_git ./secret
-cat ./secret/sonatype.sbt >> ./local.sbt
+travis_retry git clone --depth=1 $secret_git ./secret &&
+cat ./secret/sonatype.sbt >> ./local.sbt &&
 
-git log --max-count=1 --format=format:%an | xargs -0 -n 1 git config --global -- user.name
-git log --max-count=1 --format=format:%ae | xargs -0 -n 1 git config --global -- user.email
+git log --max-count=1 --format=format:%an | xargs -0 -n 1 git config --global -- user.name &&
+git log --max-count=1 --format=format:%ae | xargs -0 -n 1 git config --global -- user.email &&
 
-git config --global push.default simple
+git config --global push.default simple &&
 
-git branch --force "$TRAVIS_BRANCH"
-git checkout "$TRAVIS_BRANCH"
-git config remote.origin.url git@github.com:"$TRAVIS_REPO_SLUG".git
+git branch --force "$TRAVIS_BRANCH" &&
+git checkout "$TRAVIS_BRANCH" &&
+git config remote.origin.url git@github.com:"$TRAVIS_REPO_SLUG".git &&
 
-eval "$(ssh-agent -s)"
-ssh-add ./secret/id_rsa
+eval "$(ssh-agent -s)" &&
+ssh-add ./secret/id_rsa &&
 
 sbt "release with-defaults"
